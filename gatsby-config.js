36e65d52995bc0f00e2c5caf5cd1e6a0f9818bad
Nguyen Long Nhat <27698189+torn4dom4n@users.noreply.github.com>
2022-07-21 23:44:56 +0700

    first commit

---
+++ gatsby-config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  pathPrefix: "/the-old-book",
+  siteMetadata: {
+    // See all options: https://github.com/LekoArts/gatsby-themes/blob/master/themes/gatsby-theme-emilia-core/gatsby-config.js
+    siteTitle: `The Old Book`,
+    siteTitleAlt: `The Old Book | Vintage photos by Nhat Nguyen`,
+    siteHeadline: `The Old Book | Vintage photos by Nhat Nguyen`,
+    siteUrl: `https://the-old-book.netlify.app`,
+    siteDescription: "Vintage photos by Nhat Nguyen",
+    siteLanguage: `en`,
+    author: `@torn4dom4n`,
+  },
+  plugins: [
+    {
+      resolve: `@lekoarts/gatsby-theme-emilia`,
+      options: {
+        name: `the-old-book`,
+        socialMedia: [{}],
+        location: `Vietnam`,
+      },
+    },
+  ],
+}
